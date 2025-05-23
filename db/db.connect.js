a0908d8c00785d0653a06b664cb667c2145ffe85
Mainak <mainakhalder99@gmail.com>
2025-05-23 11:39:11 +0530

    fix: connect db

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,17 +1,30 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoUri = process.env.MONGODB;
+// const mongoUri = process.env.MONGODB;
+
+// const initializeDatabase = async () => {
+//   await mongoose
+//     .connect(mongoUri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 30000,
+//     })
+//     .then(() => console.log("Connected to database"))
+//     .catch((error) => console.log("Error connecting the database ", error));
+// };
 
 const initializeDatabase = async () => {
-  await mongoose
-    .connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000,
-    })
-    .then(() => console.log("Connected to database"))
-    .catch((error) => console.log("Error connecting the database ", error));
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB);
+    if (connection) {
+      console.log("DB connection successfully");
+    } else {
+      console.log("DB Connection failed");
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 module.exports = { initializeDatabase };
